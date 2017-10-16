d7e49b04924f1aa3333af54e54c3617f2de55b17
Hendrix Demers <hendrix.demers@mail.mcgill.ca>
2017-10-16 14:47:54 -0400

    Test function pointer to select model
    Remove trajectory result image.

--- simulation/models/energyloss/stoppingpower_test.go
+++ simulation/models/energyloss/stoppingpower_test.go
@@ -15,23 +15,15 @@ func BenchmarkStoppingPowerWithBetheJoyLuo1989(b *testing.B) {
 	}
 }
 
-func BenchmarkStoppingPowerWithBethe1930(b *testing.B) {
+func BenchmarkStoppingPowerWithBetheJoyLuo1989FuncPointer(b *testing.B) {
 	inputData := input.InputData{6, 12.011, 2.62, 10.0, 1.0e10, 10000, 0.0}
 	energy_keV := 20.0
 	b.ResetTimer()
 
+	var stoppingPower func(float64, input.InputData) float64
+	stoppingPower = StoppingPowerBetheJoyLuo1989
 	for i := 0; i < b.N; i++ {
-		StoppingPower(energy_keV, inputData, Bethe1930)
-	}
-}
-
-func BenchmarkStoppingPowerWithBetheRaoSahibWittry1974(b *testing.B) {
-	inputData := input.InputData{6, 12.011, 2.62, 10.0, 1.0e10, 10000, 0.0}
-	energy_keV := 20.0
-	b.ResetTimer()
-
-	for i := 0; i < b.N; i++ {
-		StoppingPower(energy_keV, inputData, BetheRaoSahibWittry1974)
+		stoppingPower(energy_keV, inputData)
 	}
 }
 
@@ -45,6 +37,28 @@ func BenchmarkStoppingPowerBetheJoyLuo1989(b *testing.B) {
 	}
 }
 
+func BenchmarkStoppingPowerWithBethe1930(b *testing.B) {
+	inputData := input.InputData{6, 12.011, 2.62, 10.0, 1.0e10, 10000, 0.0}
+	energy_keV := 20.0
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		StoppingPower(energy_keV, inputData, Bethe1930)
+	}
+}
+
+func BenchmarkStoppingPowerWithBethe1930FuncPointer(b *testing.B) {
+	inputData := input.InputData{6, 12.011, 2.62, 10.0, 1.0e10, 10000, 0.0}
+	energy_keV := 20.0
+	b.ResetTimer()
+
+	var stoppingPower func(float64, input.InputData) float64
+	stoppingPower = StoppingPowerBethe1930
+	for i := 0; i < b.N; i++ {
+		stoppingPower(energy_keV, inputData)
+	}
+}
+
 func BenchmarkStoppingPowerBethe1930(b *testing.B) {
 	inputData := input.InputData{6, 12.011, 2.62, 10.0, 1.0e10, 10000, 0.0}
 	energy_keV := 20.0
@@ -52,6 +66,28 @@ func BenchmarkStoppingPowerBethe1930(b *testing.B) {
 
 	for i := 0; i < b.N; i++ {
 		StoppingPowerBethe1930(energy_keV, inputData)
+	}
+}
+
+func BenchmarkStoppingPowerWithBetheRaoSahibWittry1974(b *testing.B) {
+	inputData := input.InputData{6, 12.011, 2.62, 10.0, 1.0e10, 10000, 0.0}
+	energy_keV := 20.0
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		StoppingPower(energy_keV, inputData, BetheRaoSahibWittry1974)
+	}
+}
+
+func BenchmarkStoppingPowerWithBetheRaoSahibWittry1974FuncPointer(b *testing.B) {
+	inputData := input.InputData{6, 12.011, 2.62, 10.0, 1.0e10, 10000, 0.0}
+	energy_keV := 20.0
+	b.ResetTimer()
+
+	var stoppingPower func(float64, input.InputData) float64
+	stoppingPower = StoppingPowerBetheRaoSahibWittry1974
+	for i := 0; i < b.N; i++ {
+		stoppingPower(energy_keV, inputData)
 	}
 }
 
